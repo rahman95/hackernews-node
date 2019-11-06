31017e80ab54ed7d2ca552dbab03de55e999c1e0
Rahman Younus <rahman.younus@dazn.com>
2019-11-06 13:53:02 +0000

    added more type defs and updated resolver

--- src/index.js
+++ src/index.js
@@ -3,14 +3,56 @@ const { GraphQLServer } = require("graphql-yoga");
 // defines a simple Query type with one field called info which is of type of string and can never be null
 const typeDefs = `
 type Query {
-  info: String!
+    info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+    user: User!
+}
+
+type User {
+    id: ID!
+    name: String!
+    email: String!
 }
 `;
 
 // resolver has structure of the type definition
+let user1 = {
+  id: "user-1",
+  name: "Rahman Younus",
+  email: "rahman_younus@live.co.uk"
+};
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+    user: user1
+  }
+];
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  },
+
+  User: {
+    id: parent => parent.id,
+    name: parent => parent.name,
+    email: parent => parent.email
   }
 };
 
