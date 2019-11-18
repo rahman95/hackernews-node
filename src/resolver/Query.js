7495acb5e0466b1c4c675a7a70b45dbc1db7799d
Rahman Younus <rahman.younus@dazn.com>
2019-11-18 15:37:00 +0000

    added filter

--- src/resolver/Query.js
+++ src/resolver/Query.js
@@ -2,8 +2,24 @@ function info() {
   return `This is the API of a Hackernews Clone`;
 }
 
-function feed(parent, args, context, info) {
-  return context.prisma.links();
+async function feed(parent, args, context, info) {
+  const whereFilter = getFilter(args.filter);
+
+  const links = await context.prisma.links({
+    where: whereFilter
+  });
+
+  return links;
+}
+
+function getFilter(filter) {
+  if (filter.length === 0) {
+    return {};
+  }
+
+  return {
+    OR: [{ description_contains: filter }, { url_contains: filter }]
+  };
 }
 
 module.exports = {
